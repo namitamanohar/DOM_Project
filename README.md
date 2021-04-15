916276ebfd77ac79ab6a4d166d10150c16d25e8b
Namita Manohar <57641506+namitamanohar@users.noreply.github.com>
2021-04-14 21:13:45 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# DOM_Project123
-Created with CodeSandbox
+# DOM_Project
+
+## Student Instructions
+
+- For this project, you will be looking at the starter code
+
+## Resources
+
+- https://www.w3schools.com/howto/howto_js_toggle_class.asp
+
+## Instructions
+
+1. Clone this repo using codesandbox
+2. Open a new project at https://codesandbox.io/dashboard/home
+3. Click Create Sandbox at the top right and select Import Project from the left side
+4. Paste the url of this project and click Import and Fork
+5. Click on the Github Cat on the left side
+6. Under Export to new Github Repository: give your repo the name Unit6DOMProject (your project will now be linked to your github)
+7. Click Link to Sandbox
+8. Follow the steps in the comments in the index.js code
+
+## Video of Exemplar Project
+
+![Static Template](https://user-images.githubusercontent.com/57641506/114063524-b541b300-9866-11eb-931e-5143ef604bea.gif)
---
+++ scripts.js
@@ -0,0 +1,64 @@
+function showFact() {
+  //this is for the Click Here button
+  //get the div below the click here button using the id
+  //
+  //create a p tag element and set it equal to a variable
+  //
+  // add innerHTML to the p element and set it equal to a random fact  //
+  //append this p element to the div element
+  //
+}
+
+function removeFact() {
+  //this is for the Remove Fact button
+  //get the div element below the click here button using the id and set it equal to variable
+  var factDiv = document.getElementById("FactBox");
+  //https://www.w3schools.com/jquery/html_removeclass.asp
+  //using the resource above remove the the class border
+  factDiv.classList.remove("border");
+  //using the resource above remove the the class bernie
+  factDiv.classList.remove("bernie");
+  //set the innerHTML of the div element to "" (empty strings)
+  factDiv.innerHTML = "";
+}
+
+function addBlueBackground() {
+  //this is for the Toggle Blue Background button
+  //get the div element below the click here button using the id and set it to variable
+  var factDiv = document.getElementById("FactBox");
+  // https://www.w3schools.com/howto/howto_js_toggle_class.asp
+  //using the resource above toggle the class blue from the element above
+  factDiv.classList.toggle("blue");
+}
+
+function addWhiteText() {
+  //this is for the Toggle White Text Button
+  //get the div element below the click here button using the id and set it to variable
+  var factDiv = document.getElementById("FactBox");
+  // https://www.w3schools.com/howto/howto_js_toggle_class.asp
+  //using the resource above toggle the class white from the element above
+  factDiv.classList.toggle("white");
+}
+
+function addRedBorder() {
+  //this is for the Toggle Red Border Button
+  //get the div element below the click here button using the id and set it to variable
+  var factDiv = document.getElementById("FactBox");
+  // https://www.w3schools.com/howto/howto_js_toggle_class.asp
+  //using the resource above toggle the class border from the element above
+  factDiv.classList.toggle("border");
+}
+//get the Toggle Bernie button and store into the variable called bernieBtn
+const bernieBtn = document.querySelector("#bernieBtn");
+//here is the code for adding an eventListenter to the bernieBtn that alerts a message
+bernieBtn.addEventListener("click", () => {
+  alert("A smile - courtesy of Bernie 😃🧤");
+});
+function addBernie() {
+  //this is for the Toggle Bernie button
+  //get the div element below the click here button using the id and set it to variable
+  var factDiv = document.getElementById("FactBox");
+  // https://www.w3schools.com/howto/howto_js_toggle_class.asp
+  //using the resource above toggle the class bernie from the element above
+  factDiv.classList.toggle("bernie");
+}
