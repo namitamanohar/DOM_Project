af7c82ca764c1e95853feb3013964cbb05d82647
Namita Manohar <57641506+namitamanohar@users.noreply.github.com>
2021-04-14 21:18:51 -0400

    added instructions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function showFact() {
   //this is for the Click Here button
-  //get the div below the click here button using the id
+  //get the div below the Click Here button using the id
   //
   //create a p tag element and set it equal to a variable
   //
@@ -11,7 +11,7 @@ function showFact() {
 
 function removeFact() {
   //this is for the Remove Fact button
-  //get the div element below the click here button using the id and set it equal to variable
+  //get the div element below the Click Here button using the id and set it equal to a variable
   var factDiv = document.getElementById("FactBox");
   //https://www.w3schools.com/jquery/html_removeclass.asp
   //using the resource above remove the the class border
@@ -24,7 +24,7 @@ function removeFact() {
 
 function addBlueBackground() {
   //this is for the Toggle Blue Background button
-  //get the div element below the click here button using the id and set it to variable
+  //get the div element below the Click Here button using the id and set it equal to a variable
   var factDiv = document.getElementById("FactBox");
   // https://www.w3schools.com/howto/howto_js_toggle_class.asp
   //using the resource above toggle the class blue from the element above
@@ -33,7 +33,7 @@ function addBlueBackground() {
 
 function addWhiteText() {
   //this is for the Toggle White Text Button
-  //get the div element below the click here button using the id and set it to variable
+  //get the div element below the Click Here button using the id and set it equal to a variable
   var factDiv = document.getElementById("FactBox");
   // https://www.w3schools.com/howto/howto_js_toggle_class.asp
   //using the resource above toggle the class white from the element above
@@ -42,13 +42,13 @@ function addWhiteText() {
 
 function addRedBorder() {
   //this is for the Toggle Red Border Button
-  //get the div element below the click here button using the id and set it to variable
+  //get the div element below the Click Here button using the id and set it equal to a variable
   var factDiv = document.getElementById("FactBox");
   // https://www.w3schools.com/howto/howto_js_toggle_class.asp
   //using the resource above toggle the class border from the element above
   factDiv.classList.toggle("border");
 }
-//get the Toggle Bernie button and store into the variable called bernieBtn
+//get the Toggle Bernie button using its id and set it equal to the variable called bernieBtn
 const bernieBtn = document.querySelector("#bernieBtn");
 //here is the code for adding an eventListenter to the bernieBtn that alerts a message
 bernieBtn.addEventListener("click", () => {
@@ -56,7 +56,7 @@ bernieBtn.addEventListener("click", () => {
 });
 function addBernie() {
   //this is for the Toggle Bernie button
-  //get the div element below the click here button using the id and set it to variable
+  //get the div element below the Click Here button using the id and set it equal to a variable
   var factDiv = document.getElementById("FactBox");
   // https://www.w3schools.com/howto/howto_js_toggle_class.asp
   //using the resource above toggle the class bernie from the element above
